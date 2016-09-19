2c32e02c9fd27417b1ce8fb231aba83635b4ef21
Amrut <amrut@amuratech.com>
2016-09-19 18:08:05 +0530

    admin/employee layout setup

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,3 +1,8 @@
 class Resource < ActiveRecord::Base
 	belongs_to :company
+
+
+	validates :name, presence: :true
+	validates :count , presence: :true, numericality: {greater_than: 0, only_integer: true}
+
 end
