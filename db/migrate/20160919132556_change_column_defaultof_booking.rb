e61a247d07658ba45595eddb0ee00f37a5be02af
Amrut <amrut@amuratech.com>
2016-09-19 19:51:01 +0530

    default update of bookings table

--- db/migrate/20160919132556_change_column_defaultof_booking.rb
+++ db/migrate/20160919132556_change_column_defaultof_booking.rb
@@ -1,7 +1,15 @@
 class ChangeColumnDefaultofBooking < ActiveRecord::Migration
-  def change
-  	change_column :bookings, :status, :boolean
-  	change_column_default :bookings, :shift, from: nil, to: false
-  	change_column_default :bookings, :status, from: nil, to: 0
+
+  def up 
+  	change_column :bookings, :shift, :boolean
+  	change_column_default :bookings, :status, 0
+  	change_column_default :bookings, :shift, false
+  end
+
+  def down
+  	change_column :bookings, :shift, :integer
+  	change_column :bookings, :status, :integer
+  	change_column_default :bookings, :shift, nil
+  	change_column_default :bookings, :status, nil
   end
 end
