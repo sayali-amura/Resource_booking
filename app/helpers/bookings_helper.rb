aee9a8b479930173497abe9ca35f462d25e26cb4
Amrut <amrut@amuratech.com>
2016-09-20 20:21:11 +0530

    employee booking request form

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -1,2 +1,16 @@
 module BookingsHelper
+
+def status_normalization(status_code)
+	case 
+	when status_code == 0
+		 "Request"
+	when status_code == 1
+		 "Grant"
+	when status_code == 2
+		 "Reject"
+	when status_code == 3		
+		 "Realease"
+	end
+end
+
 end
