0d237af521c66cffe9a5e0eaabb60cd49530fea2
sayali-amura <sayali.p@amuratech.com>
2016-09-20 14:45:30 +0530

    admin controller changed to CRUD employee

--- app/controllers/employees/admins_controller.rb
+++ app/controllers/employees/admins_controller.rb
@@ -1,2 +1,36 @@
 class Employees::AdminsController < ApplicationController
+	before_action :find_employee, only: [:show, :edit, :update]
+	def index
+		@employees = Employee.all
+	end
+	def new
+		@employee = employee.new
+	end
+
+	def create
+		@employee = employee.new(employee_params)
+		if @employee.save
+			redirect_to @employee
+		else
+			render :new
+		end
+	end 
+
+	def show
+	 
+	end
+	def update 
+		if @employee.update(employee_params)
+			redirect_to @employee
+		else
+			render :edit
+		end
+	end
+	private
+	def employee_params
+		params.require(:employee).permit(:name, :email, :password_digest, :date_of_joining, :role_id, :manager_id)
+	def find_employee
+		@employee = Employee.find(params[:id])
+	end
+ 
 end
