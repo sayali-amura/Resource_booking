46debb30253ee191bc98deb3f00c138e80b81fb5
sayali-amura <sayali.p@amuratech.com>
2016-09-21 17:23:16 +0530

    complaint controller and view changes

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -1,11 +1,44 @@
 class ComplaintsController < ApplicationController
-  def index
-  	@complaints = Complaint.all
-  end
+  before_action :find_complaint, only: [:show, :edit, :update]
+  before_action :list_resources, only: [:new,:edit]
+	def index
+		@complaints = Complaint.all
+		
+	end
+	def new
+		@complaint = Complaint.new
 
-  def show
-  end
-
-  def new
-  end
+	end
+	def create
+		complaint_param = complaint_params
+		complaint_param[:status] = 0
+		#add employee id to complaint_param after devise setup
+		#complaint_param[:employee_id] = current_user.id
+		@complaint = Complaint.new(complaint_param)
+		if @complaint.save
+			redirect_to @complaint 
+		else 
+			render :new
+		end
+	end
+	def show; end
+	def edit; end
+	def update
+		if @complaint.update(complaint_params)
+			redirect_to @complaint
+		else
+			render :edit
+		end
+	end
+	private
+	def complaint_params
+		params.require(:complaint).permit(:comment, :resource_id, :employee_id)
+	end
+	def find_complaint
+		@complaint = complaint.find(params[:id])
+		@resources = Resource.all
+	end
+	def list_resources
+		@resources = Resource.all
+	end
 end
