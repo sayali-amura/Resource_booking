75caf16afec78277961c2d1677c2c42df4fe047b
Amrut <amrut@amuratech.com>
2016-09-21 13:13:59 +0530

    start & end time for company

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921073740) do
+ActiveRecord::Schema.define(version: 20160921074314) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160921073740) do
     t.string   "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "start_time"
+    t.float    "end_time"
   end
 
   create_table "complaints", force: :cascade do |t|
