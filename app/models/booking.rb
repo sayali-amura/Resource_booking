f6ceac34751d8d10d57d6ff5088e9f1fafc86dbb
Amrut <amrut@amuratech.com>
2016-09-21 16:47:06 +0530

    booking belongs_to resource

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,7 @@
 class Booking < ActiveRecord::Base
 	belongs_to :employee
+	belongs_to :resource
 	has_many	:messages, as: :property
-	validates :duration, presence: true
+	validates :slot,:date_of_booking,:comment , presence: true
 	validates :priority ,inclusion: {in:[0,1,2]}	
 end
