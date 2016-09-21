88f72478addeb6bf6feaca62f6798d6e289aeda9
sayali-amura <sayali.p@amuratech.com>
2016-09-21 12:15:59 +0530

    resource_id to bookings

--- app/helpers/bookings_helper.rb
+++ app/helpers/bookings_helper.rb
@@ -12,6 +12,17 @@ def status_normalization(status_code)
 		 "Realease"
 	end
 end
+def priority_normalization(priority_code)
+	case 
+	when priority_code == 0
+		 "Low"
+	when priority_code == 1
+		 "Medium"
+	when priority_code == 2
+		 "High"
+	end
+end
+
 
 def check_reply booking, status_code
 	(!booking.reply.nil?) && (status_code==1 || status_code ==2)
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160921054057) do
+ActiveRecord::Schema.define(version: 20160921063745) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20160921054057) do
     t.integer  "employee_id"
     t.date     "date_of_booking"
     t.float    "duration"
+    t.integer  "resource_id"
   end
 
   add_index "bookings", ["employee_id"], name: "index_bookings_on_employee_id", using: :btree
