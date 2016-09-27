c8491f5ee5cc0a900595b7e9627280710c42e76f
Amrut <amrut@amuratech.com>
2016-09-27 19:37:03 +0530

    respec for booking

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do 
+  	@booking = Booking.new({comment: "hello",feedback: "",employee_id:6,date_of_booking: '2016-09-27',priority:0,company_id: 2, slot: 0,resource_id: 3})
+  end
+  context "field validations" do 
+  	it "check is valid" do 
+  		expect(@booking.save).to eq(true)
+  	end
+  end
 end
