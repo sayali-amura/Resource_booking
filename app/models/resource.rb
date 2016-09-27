0eea519bd750e9afaf252e8b333ffb69a5d7cacd
Amrut <amrut@amuratech.com>
2016-09-27 15:34:58 +0530

    timeslot of resources

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,7 +1,27 @@
 class Resource < ActiveRecord::Base
+
+
 	belongs_to :company
 	has_many :bookings
 	has_many :complaints
 	validates :count, numericality: { only_integer: true, greater_than: 0 }
 	validates :name, :count, presence: true
+
+
+	def timeslots
+		company = self.company
+		start_time = company.start_time
+		end_time = company.end_time
+		company_duration = end_time - start_time
+		no_of_slots = ( company_duration / self.time_slot ).to_i
+		time_slot_array =Array.new
+		no_of_slots.times do | index |
+			time_slot_array << ["#{index+start_time}-#{index+start_time+self.time_slot}", index]
+		end
+		time_slot_array
+	end
+
+
+
+
 end
