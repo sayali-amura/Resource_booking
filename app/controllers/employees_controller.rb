35d96d43b152a2e5ebea593621e93b1f098fa53d
Amrut <amrut@amuratech.com>
2016-09-27 15:41:19 +0530

    added before filter

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,9 +1,26 @@
 class EmployeesController < ApplicationController
+
+	before_action :find_employee
+
 	def index
 		if !employee_signed_in?
 			redirect_to new_employee_session_path
 		elsif current_employee.role_id ==0
 			redirect_to admin_employees_path
 		end
+		@bookings = @employee.bookings
+		@complaints = @employee.complaints
 	end
+
+	
+
+	private
+	def find_employee
+		if employee_signed_in?
+			@employee = current_employee
+		else
+			redirect_to root_path
+		end
+	end
+
 end
