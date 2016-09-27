2882a40a4434eda099a70bed270696d99a7e1ad1
Amrut <amrut@amuratech.com>
2016-09-27 17:37:11 +0530

    before validation filter

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -12,16 +12,20 @@ class Booking < ActiveRecord::Base
 	validate :is_resource_available?, on: :index
 
 	before_save :add_company_id
+	before_validation 	:ensure_date_has_value
 
+
+
+	protected
 
 	def is_date_valid?
-		unless self.date_of_booking >= Time.zone.now.beginning_of_day
+		unless self.date_of_booking >= Time.zone.now.beginning_of_day 
 			self.errors[:date_validation] << "You can't book resource for previous day"
 		end
 	end
 
 	def check_holiday?
-		unless self.date_of_booking.wday!=7  
+		unless self.date_of_booking.wday!=7 
 			self.errors[:day_validation] << "Booking can't be done on holidays"
 		end
 	end
@@ -38,7 +42,7 @@ class Booking < ActiveRecord::Base
 	end
 
 	def slot_valid? 
-		if self.slot > self.resource.available_time_slot(self.date_of_booking).length
+		if self.slot.to_i > self.resource.available_time_slot(self.date_of_booking).length
 			self.errors[:slot_invalid] << "This slot is invalid"
 		end
 	end
@@ -56,7 +60,12 @@ class Booking < ActiveRecord::Base
 		end
 	end
 
-	protected
+	def ensure_date_has_value
+		unless !self.date_of_booking.blank?
+			self.errors[:date_of_booking] << "=>Date of booking can't be empty"
+			raise "date_of_booking empty"
+		end
+	end	
 
 	def add_company_id
 		company = self.employee.company
