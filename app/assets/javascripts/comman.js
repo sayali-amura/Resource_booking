8997d6159d4a17ed63451c088407d40f7eec3e3f
Amrut <amrut@amuratech.com>
2016-09-29 16:41:15 +0530

    invalid date validation on client side

--- app/assets/javascripts/comman.js
+++ app/assets/javascripts/comman.js
@@ -1,6 +1,6 @@
 $(document).on('turbolinks:load',function() {
     $('#booking_resource_id').change(function() {
-      resource_name = $("#booking_resource_id option:selected").text(),
+      resource_name = $("#booking_resource_id option:selected").text();
       //alert(resource_name),
         $.get(
           "/resource_time_slot/"+resource_name,
@@ -11,18 +11,30 @@ $(document).on('turbolinks:load',function() {
         );
     });
 
-
      $('#booking_date_of_booking').change(function() {
-      resource_name = $("#booking_resource_id option:selected").text(),
-      date_of_booking = $("#booking_date_of_booking").val(),
-        $.get(
-          "/booking_date_slots/"+resource_name+"/date_of_booking="+date_of_booking,
-           function(data,success) {
-             console.log(data);
-             $("#booking_slot").html(data.replace(/[\t\n\\]+/g,''));
-           }
-        );
+      
+      date_of_booking = new Date($("#booking_date_of_booking").val());
+      present_date = new Date();
+      if (date_of_booking.getDate() >= present_date.getDate() && date_of_booking.getMonth() >= present_date.getMonth() && date_of_booking.getFullYear() >= present_date.getFullYear() ) 
+      {
+        resource_name = $("#booking_resource_id option:selected").text();
+          $.get(
+            "/booking_date_slots/"+resource_name+"/date_of_booking="+$("#booking_date_of_booking").val(),
+             function(data,success) {
+               console.log(data);
+               $("#booking_slot").html(data.replace(/[\t\n\\]+/g,''));
+             }
+          );
+      }
+      else
+      {
+        $("#form-errors").html("<div class='alert alert-danger'> Select valid date</div>");
+      }
     });
+
+     // $('#datepicker').datepicker({
+     //  startDate: new Date()
+     // });
 
 
   });
