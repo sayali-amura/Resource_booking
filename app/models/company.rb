a29b16e1cfe63b24444ec94a3ed377d3a6562214
Amrut <amrut@amuratech.com>
2016-09-30 10:31:34 +0530

    company has_many complaints

--- app/models/company.rb
+++ app/models/company.rb
@@ -12,6 +12,7 @@ class Company < ActiveRecord::Base
 	has_many :resources
 	has_many :employees
 	has_many :bookings
+	has_many :complaints
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 255 }, format: {with: VALID_EMAIL_REGEX },
 																uniqueness: { case_sensitive: false }
