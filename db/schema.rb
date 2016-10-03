ba72aac6c49a59d04368434cbd39d888925811f9
Amrut <amrut@amuratech.com>
2016-10-03 12:45:50 +0530

    remove priority from bookings

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930091642) do
+ActiveRecord::Schema.define(version: 20161003070839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,7 +20,6 @@ ActiveRecord::Schema.define(version: 20160930091642) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.integer  "status",          default: 0
-    t.integer  "priority"
     t.string   "feedback"
     t.string   "comment"
     t.integer  "employee_id"
