4f17e9d39488803cba1c566dced48bb5357d5f38
sayali-amura <sayali.p@amuratech.com>
2016-10-03 12:50:46 +0530

    drop down for role and manager

--- app/helpers/admin/employees_helper.rb
+++ app/helpers/admin/employees_helper.rb
@@ -2,4 +2,18 @@ module Admin::EmployeesHelper
 	def company_domain
 		"   @"<<current_employee.company.name<<".com"
 	end
+	def list_roles
+		roles = current_employee.company.roles
+		@roles = []
+		roles.each do | role |
+			@roles << [role.designation, role.id]
+		end
+	end
+	def list_employees
+		employees = current_employee.company.employees
+		@employees = []
+		employees.each do | emp |
+			@employees << [emp.name, emp.id]
+		end
+	end
 end
