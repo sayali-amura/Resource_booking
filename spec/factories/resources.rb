7a0cf7da8f2501a051ce0637d427f794a9b946f4
sayali-amura <sayali.p@amuratech.com>
2016-10-05 17:58:55 +0530

    minute

--- spec/factories/resources.rb
+++ spec/factories/resources.rb
@@ -2,9 +2,9 @@ require 'faker'
 require 'as-duration'
 
 FactoryGirl.define do
-	factory :resource do |r|
-		r.name {Faker::Lorem.word}
-		r.time_slot {1.hour}
+	factory :resource do
+		name {Faker::Lorem.word}
+		time_slot {1.hour}
 		company
 	end
 end 
