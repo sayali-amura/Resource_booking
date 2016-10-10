cd1eca7662711ad8215c50b3be59a8de31080838
Amrut <amrut@amuratech.com>
2016-10-10 15:14:51 +0530

    bookings methods specs

--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -3,21 +3,17 @@ require 'rails_helper'
 RSpec.describe Booking, type: :model do
 
   before(:each) do 
-  	@company = Company.new(name: "hello1",email: "hello2@gmail.com",phone: "+911254567890",start_time:"9:00",end_time:"18:00")
-  	@company.save
-  	@resource = @company.resources.create(name: "ac", time_slot:"1:00")
-
-  	@employee = @company.employees.first
+  	@company = create(:company)
   	@resource = @company.resources.first
-  	@booking = @employee.bookings.new({comment: "hello",feedback: "",employee_id:6,date_of_booking: '2016-10-28', slot: 0})
+  	@employee = @company.employees.first
+  	@booking = @employee.bookings.build({comment: "hello",feedback: "",employee_id:6,date_of_booking: '2016-10-28', slot: 0})
   	@booking.resource_id = @resource.id
   end
 
-
+  
   context "field validations" do 
   	context "date" do 
 	  	it "previous date" do 
-	  		puts @company.inspect
 	  		@booking.date_of_booking = '2016-09-12'
 	  		expect(@booking).to_not be_valid
 	  	end
@@ -25,9 +21,6 @@ RSpec.describe Booking, type: :model do
 	  		@booking.date_of_booking = ""
 	  		@booking.errors
 	  		expect(@booking).to_not be_valid
-	  	end
-	  	it "future date" do
-	  		expect(@booking).to be_valid
 	  	end
 	  	it "holiday" do 
 	  		@booking.date_of_booking = '2016-09-02'
@@ -64,7 +57,99 @@ RSpec.describe Booking, type: :model do
 		end
 	end
 
-
   end
+
+  context "instance methods" do 
+		context "positive" do 
+			context "#is_resource_added_to_company" do 
+				it "company has resource" do
+
+					expect(@booking.send(:is_resource_added_to_company?)).to eq(true)
+				end
+				it "company don't have resource" do 
+					@company.resources.destroy_all
+					expect(@booking.send(:is_resource_added_to_company?)).to eq(false)
+				end
+			end
+			context "#is_slot_already_passed?" do 
+				it "date is today and slot is already passed  " do 
+					@booking.date_of_booking = Date.today
+					@booking.slot =  0
+					error_check "is_slot_already_passed?", "to","=> This slot is already passed"
+				end
+				it "date is not today " do
+					@booking.date_of_booking = Date.today+3
+					@booking.slot = 0
+					@booking.send(:is_slot_already_passed?)
+					expect(@booking.errors.any?).to eq(false)
+				end
+			end
+			context "#check_holiday?" do 
+				it "date_of_booking is holiday" do 
+					@booking.date_of_booking = "2016-10-16"
+					error_check "check_holiday?", "to","=> Booking can't be done on holidays"
+				end
+				it "date_of_booking is not holiday" do 
+					@booking.date_of_booking = Date.today
+					error_check "check_holiday?", "to_not", "=> Booking can't be done on holidays"
+				end
+			end
+			context "#slot_valid?" do
+				it "booking slot is present in resource timeslots" do 
+					error_check "slot_valid?","to_not", "=> This slot is invalid" 
+				end
+				it "booking slot is not present in resource timeslots" do 
+					@booking.slot = -9
+					error_check "slot_valid?","to", "=> This slot is invalid"
+				end
+			end
+			context "#is_date_valid?" do 
+				it "past date" do 
+					@booking.date_of_booking = "2016-03-13"
+					error_check "is_date_valid?", "to", "=> You can't book resource for previous day"
+				end
+				it "future date" do 
+					@booking.date_of_booking = "2016-12-13"
+					error_check "is_date_valid?", "to_not", "=> You can't book resource for previous day"
+				end
+			end
+			context "#is_slot_alloted?" do 
+				it "no bookings" do 
+					error_check "is_slot_alloted?","to_not","=> This slot is already alloted"
+				end
+				it "new record with different slot" do 
+					@booking.save
+					@booking = @employee.bookings.build({comment: "hello",feedback: "",employee_id:6,date_of_booking: "2016-10-28", slot: 1})
+					error_check "is_slot_alloted?","to_not","=> This slot is already alloted"
+				end
+				it "old record with different slot" do 
+					@booking.save
+					@booking.slot = 1
+					error_check "is_slot_alloted?", "to_not","=> This slot is already alloted"
+				end
+				it "old record but same slot" do 
+					@booking.save
+					@booking.slot = 0
+					error_check "is_slot_alloted?", "to_not","=> This slot is already alloted"
+				end
+				it "new record but already alloted slot" do
+					@booking.save
+					@booking = @employee.bookings.build({comment: "hello",feedback: "",employee_id:6,date_of_booking: '2016-10-28', slot: 0})
+  					@booking.resource_id = @resource.id
+  					error_check "is_slot_alloted?","to","=> This slot is already alloted"
+				end
+			end
+			def error_check method,expectation, error_msg
+				@booking.send(method.to_sym)
+				eval("expect(@booking.errors.full_messages).#{expectation} include(/#{error_msg}/)")
+			end
+			def print_all
+				p @company.inspect
+				p "employee are #{@company.employees.inspect}"
+				p "resources are #{@company.resources.inspect}"
+				p "bookings are #{@company.bookings.inspect}"
+			end
+		end
+	end
 
 end
