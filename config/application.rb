0da19581adce705620b2ed40434b1b0c5b2d2452
Amrut <amrut@amuratech.com>
2016-11-07 12:15:57 +0530

    remove require/all

--- config/application.rb
+++ config/application.rb
@@ -3,8 +3,7 @@ require File.expand_path('../boot', __FILE__)
 # require 'rails/all'
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "active_resource/railtie" # Comment this line for Rails 4.0+
-require "rails/test_unit/railtie"
+require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
