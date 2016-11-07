ce17ae19b8aca5a8505c761a80ec2c84d50981dc
Amrut <amrut@amuratech.com>
2016-11-07 12:13:36 +0530

    comment  file

--- db/schema.rb
+++ db/schema.rb
@@ -1,116 +1,116 @@
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended that you check this file into your version control system.
+# # encoding: UTF-8
+# # This file is auto-generated from the current state of the database. Instead
+# # of editing this file, please use the migrations feature of Active Record to
+# # incrementally modify your database, and then regenerate this schema definition.
+# #
+# # Note that this schema.rb definition is the authoritative source for your
+# # database schema. If you need to create the application database on another
+# # system, you should be using db:schema:load, not running all the migrations
+# # from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# # you'll amass, the slower it'll run and the greater likelihood for issues).
+# #
+# # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006074139) do
+# ActiveRecord::Schema.define(version: 20161006074139) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+#   # These are extensions that must be enabled in order to support this database
+#   enable_extension "plpgsql"
 
-  create_table "bookings", force: :cascade do |t|
-    t.datetime "created_at",                  null: false
-    t.datetime "updated_at",                  null: false
-    t.integer  "status",          default: 0
-    t.string   "feedback"
-    t.string   "comment"
-    t.integer  "employee_id"
-    t.date     "date_of_booking"
-    t.integer  "resource_id"
-    t.integer  "slot"
-    t.integer  "company_id"
-  end
+#   create_table "bookings", force: :cascade do |t|
+#     t.datetime "created_at",                  null: false
+#     t.datetime "updated_at",                  null: false
+#     t.integer  "status",          default: 0
+#     t.string   "feedback"
+#     t.string   "comment"
+#     t.integer  "employee_id"
+#     t.date     "date_of_booking"
+#     t.integer  "resource_id"
+#     t.integer  "slot"
+#     t.integer  "company_id"
+#   end
 
-  add_index "bookings", ["employee_id"], name: "index_bookings_on_employee_id", using: :btree
+#   add_index "bookings", ["employee_id"], name: "index_bookings_on_employee_id", using: :btree
 
-  create_table "companies", force: :cascade do |t|
-    t.string   "name"
-    t.string   "email"
-    t.string   "phone"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.time     "start_time"
-    t.time     "end_time"
-  end
+#   create_table "companies", force: :cascade do |t|
+#     t.string   "name"
+#     t.string   "email"
+#     t.string   "phone"
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.time     "start_time"
+#     t.time     "end_time"
+#   end
 
-  add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
-  add_index "companies", ["phone"], name: "index_companies_on_phone", unique: true, using: :btree
+#   add_index "companies", ["email"], name: "index_companies_on_email", unique: true, using: :btree
+#   add_index "companies", ["phone"], name: "index_companies_on_phone", unique: true, using: :btree
 
-  create_table "complaints", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "comment"
-    t.integer  "status"
-    t.integer  "resource_id"
-    t.integer  "employee_id"
-    t.integer  "company_id"
-  end
+#   create_table "complaints", force: :cascade do |t|
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
+#     t.string   "comment"
+#     t.integer  "status"
+#     t.integer  "resource_id"
+#     t.integer  "employee_id"
+#     t.integer  "company_id"
+#   end
 
-  add_index "complaints", ["employee_id"], name: "index_complaints_on_employee_id", using: :btree
+#   add_index "complaints", ["employee_id"], name: "index_complaints_on_employee_id", using: :btree
 
-  create_table "employees", force: :cascade do |t|
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.string   "name"
-    t.integer  "age"
-    t.date     "date_of_joining"
-    t.integer  "manager_id"
-    t.integer  "role_id"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.integer  "company_id"
-    t.string   "email"
-  end
+#   create_table "employees", force: :cascade do |t|
+#     t.string   "encrypted_password",     default: "", null: false
+#     t.string   "reset_password_token"
+#     t.datetime "reset_password_sent_at"
+#     t.datetime "remember_created_at"
+#     t.integer  "sign_in_count",          default: 0,  null: false
+#     t.datetime "current_sign_in_at"
+#     t.datetime "last_sign_in_at"
+#     t.string   "current_sign_in_ip"
+#     t.string   "last_sign_in_ip"
+#     t.string   "confirmation_token"
+#     t.datetime "confirmed_at"
+#     t.datetime "confirmation_sent_at"
+#     t.string   "unconfirmed_email"
+#     t.string   "name"
+#     t.integer  "age"
+#     t.date     "date_of_joining"
+#     t.integer  "manager_id"
+#     t.integer  "role_id"
+#     t.datetime "created_at",                          null: false
+#     t.datetime "updated_at",                          null: false
+#     t.integer  "company_id"
+#     t.string   "email"
+#   end
 
-  add_index "employees", ["confirmation_token"], name: "index_employees_on_confirmation_token", unique: true, using: :btree
-  add_index "employees", ["manager_id"], name: "index_employees_on_manager_id", using: :btree
-  add_index "employees", ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
+#   add_index "employees", ["confirmation_token"], name: "index_employees_on_confirmation_token", unique: true, using: :btree
+#   add_index "employees", ["manager_id"], name: "index_employees_on_manager_id", using: :btree
+#   add_index "employees", ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true, using: :btree
 
-  create_table "messages", force: :cascade do |t|
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "content"
-    t.integer  "booking_id"
-    t.integer  "property_id"
-    t.string   "property_type"
-  end
+#   create_table "messages", force: :cascade do |t|
+#     t.datetime "created_at",    null: false
+#     t.datetime "updated_at",    null: false
+#     t.string   "content"
+#     t.integer  "booking_id"
+#     t.integer  "property_id"
+#     t.string   "property_type"
+#   end
 
-  add_index "messages", ["booking_id"], name: "index_messages_on_booking_id", using: :btree
+#   add_index "messages", ["booking_id"], name: "index_messages_on_booking_id", using: :btree
 
-  create_table "resources", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "name"
-    t.integer  "company_id"
-    t.time     "time_slot"
-  end
+#   create_table "resources", force: :cascade do |t|
+#     t.datetime "created_at", null: false
+#     t.datetime "updated_at", null: false
+#     t.string   "name"
+#     t.integer  "company_id"
+#     t.time     "time_slot"
+#   end
 
-  create_table "roles", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "designation"
-    t.string   "department"
-    t.integer  "priority"
-    t.integer  "company_id"
-  end
+#   create_table "roles", force: :cascade do |t|
+#     t.datetime "created_at",  null: false
+#     t.datetime "updated_at",  null: false
+#     t.string   "designation"
+#     t.string   "department"
+#     t.integer  "priority"
+#     t.integer  "company_id"
+#   end
 
-end
+# end
